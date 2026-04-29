751c41024f64be12a68bc3a5f465e624a6f15515
harsh.dwivedi@emotionmobility.com <harsh.dwivedi@emotionmobility.com>
2026-04-29 19:31:33 +0530

    Release 1.2.0

--- Package.swift
+++ Package.swift
@@ -5,16 +5,13 @@ let package = Package(
     name: "JailbreakShield",
     platforms: [.iOS(.v13)],
     products: [
-        .library(
-            name: "JailbreakShield",
-            targets: ["JailbreakShield"]
-        )
+        .library(name: "JailbreakShield", targets: ["JailbreakShield"])
     ],
     targets: [
         .binaryTarget(
             name: "JailbreakShield",
-            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.0/JailbreakShield.xcframework.zip",
-            checksum: "206c712371e491054b451ae54de04754c53b943d36d47f541b39f9dddb7e3ab8"
+            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.2.0/JailbreakShield.xcframework.zip",
+            checksum: "PASTE_CHECKSUM_FROM_STEP_1_HERE"
         )
     ]
 )
