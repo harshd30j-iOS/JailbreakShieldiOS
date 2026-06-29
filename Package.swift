a332e2d0f5e9540739405020d9e0b876b9f28d88
harsh.dwivedi@emotionmobility.com <harsh.dwivedi@emotionmobility.com>
2026-06-29 19:06:14 +0530

    Bump version to 1.1.6 and fix scrolling issue by using view-based reparenting for screen protection

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JailbreakShield",
-            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.5/JailbreakShield.xcframework.zip",
-            checksum: "49f8d89c90db8812064a6a3496929f2b6ffbc75c5085c532bbda871a73d00dd0"
+            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.6/JailbreakShield.xcframework.zip",
+            checksum: "708a2f8dd68a4ff63c20929bc30a9a940c8a86e24406586f49eb8a6b7bcccb97"
         )
     ]
 )
