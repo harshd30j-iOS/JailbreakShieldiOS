c93c68a2054e061d09569cb9a8d69bc1c673ee25
harsh.dwivedi@emotionmobility.com <harsh.dwivedi@emotionmobility.com>
2026-06-29 19:13:15 +0530

    Bump version to 1.1.8 and implement view controller containment containment protection with KVO root observation

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JailbreakShield",
-            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.7/JailbreakShield.xcframework.zip",
-            checksum: "c23dea14e0c922a562f9bac4624060f3c74f3f15134225fe571462afb3e9e71f"
+            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.8/JailbreakShield.xcframework.zip",
+            checksum: "eb19ce3df9cce47c1b3f91cb7522ed8a6cadab444bd66dc0f3210100ff35007e"
         )
     ]
 )
