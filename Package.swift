46f91b41996f8c82acac5132d3e4eb19d14081bd
harsh.dwivedi@emotionmobility.com <harsh.dwivedi@emotionmobility.com>
2026-06-29 19:10:23 +0530

    Bump version to 1.1.7 and implement HDS_secureTextField to fix view hierarchy alignment and hit testing

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JailbreakShield",
-            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.6/JailbreakShield.xcframework.zip",
-            checksum: "708a2f8dd68a4ff63c20929bc30a9a940c8a86e24406586f49eb8a6b7bcccb97"
+            url: "https://github.com/harshd30j-iOS/JailbreakShieldiOS/releases/download/1.1.7/JailbreakShield.xcframework.zip",
+            checksum: "c23dea14e0c922a562f9bac4624060f3c74f3f15134225fe571462afb3e9e71f"
         )
     ]
 )
